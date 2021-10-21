36c6b4d29c7f2c1bae83e5ac3b5f8ddf1cb39f00
Jakob McBroome <jmcbroome@gmail.com>
2021-10-20 17:52:54 -0700

    Add double population parameter files

---
+++ parameter-files/doublepop.mg
@@ -0,0 +1,3 @@
+#Migration_format_version 0.0.1
+0.99 0.01
+0.01 0.99 
---
+++ parameter-files/doublepop.pp
@@ -0,0 +1,4 @@
+#Population_format_version 0.0.1
+id size contactDensity conDenAfterLD startLD endLD
+0 500000000 1.0 0.1 5 1
+1 500000000 1.0 0.1 5 1
